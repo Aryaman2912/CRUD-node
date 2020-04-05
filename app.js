3af99e9f2e2d28026f57ebf6e41f04e1365eeffc
Aryaman <aryavnps@gmail.com>
2020-04-06 02:06:01 +0530

    index route and view

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var methodOverride = require("method-override"),
 mongoose.connect("mongodb://localhost/movie_review_app",{useNewUrlParser: true,useUnifiedTopology: true});
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride.("_method"));
+app.use(methodOverride("_method"));
 app.use(express.static("public"))
 
 //MONGOOSE, MODEL CONFIG
 var movieSchema  = new mongoose.Schema({
-    title: String;
-    rating: Number;
-    body: String;
+    title: String,
+    rating: Number,
+    description: String,
     created: 
             {
                 type:Date,
@@ -23,3 +23,25 @@ var movieSchema  = new mongoose.Schema({
 });
 
 var Movie = mongoose.model("Movie",movieSchema)
+
+//ROUTES
+
+app.get("/",function(req,res){
+    res.redirect("/movies");
+});
+
+//INDEX ROUTE
+app.get("/movies",function(req,res){
+    Movie.find({},function(err,movies){
+        if(err){
+            res.send("Something went wrong!");
+        }
+        else{
+            res.render("index",{movies:movies})
+        }
+    });
+});
+
+app.listen(8000,function(){
+    console.log("Server Started")
+})
