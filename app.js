58d125eccbeb0393b899ea904b18db6800236cbe
Aryaman <aryavnps@gmail.com>
2020-04-09 02:07:25 +0530

    new,create,edit,show routes and views

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.static("public"))
 var movieSchema  = new mongoose.Schema({
     title: String,
     rating: Number,
-    description: String,
+    review: String,
+    image: String,
     created: 
             {
                 type:Date,
@@ -30,7 +31,7 @@ app.get("/",function(req,res){
     res.redirect("/movies");
 });
 
-//INDEX ROUTE
+//INDEX - Home page
 app.get("/movies",function(req,res){
     Movie.find({},function(err,movies){
         if(err){
@@ -42,6 +43,62 @@ app.get("/movies",function(req,res){
     });
 });
 
+//NEW - form to render form to add new review 
+app.get("/movies/new",function(req,res){
+    res.render("new");
+});
+
+//CREATE - add review to form
+app.post("/movies/",function(req,res){
+    Movie.create(req.body.movie,function(err,newmovie){
+        if(err){
+            res.render("new");
+        }
+        else{
+            res.redirect("/movies/");
+        }
+    });
+});
+
+//SHOW - detailed review for a moview
+app.get("/movies/:id",function(req,res){
+    Movie.findById(req.params.id,function(err,foundMovie){
+        if(err){
+            res.send("Oops! Something went wrong!")
+        }
+        else{
+            res.render("show",{Movie:foundMovie})
+        }
+    });
+});
+
+app.get("/movies/:id/edit",function(req,res){
+    Movie.findById(req.params.id,function(err,movie){
+        if(err){
+            res.send("Oops! Something went wrong!")
+        }
+        else{
+            res.render("edit",{movie:movie})
+        }
+    });
+});
+
+app.put("/movies/:id",function(req,res){
+    Movie.findByIdAndUpdate(req.params.id,req.body.movie,function(err,movie){
+        if(err){
+            res.send("Oops! Something went wrong!");
+        }
+        else{
+            res.redirect("/movies/" + req.params.id)
+        }
+    })
+})
+
 app.listen(8000,function(){
     console.log("Server Started")
 })
+
+// Harry Potter and the Deathly Hallows – Part 2
+// https://upload.wikimedia.org/wikipedia/en/7/74/DeathlyHallowsGame2cover.jpg
+//Harry Potter and the Deathly Hallows part 2 was a fitting conclusion to the harry potter franchise. The emotions were brought about brilliantly by the actors and the direction was very good as well. Emma Watson was captivating as always.
+
